aac261b9fbcec2325abc1658314fd38613293c3c
ritowhid <207604574+ritowhid@users.noreply.github.com>
2025-09-29 21:58:21 +0600

    initial commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Towhid")
+
+
+let name = "Towhid"
+let age = 22
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
